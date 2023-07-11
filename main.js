875b3ff6175109a316e12dd47dee0044b8528901
Moara Miranda <moaramirandass@gmail.com>
2023-07-11 19:53:19 +0200

    Adding JS for modal behavior

--- main.js
+++ main.js
@@ -6,10 +6,37 @@ const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
 
+const Modal = {
+  wrapper: document.querySelector(".modal-wrapper"),
+  message: document.querySelector(".modal .title span"),
+  buttonClose: document.querySelector(".modal button.close"),
+
+  open() {
+    Modal.wrapper.classList.add("open");
+  },
+  close() {
+    Modal.wrapper.classList.remove("open");
+  },
+};
+
+/******************************
+Functions
+******************************/
+
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  const weight = inputWeight.value
-  const height = inputHeight.value
-  console.log(weight, height)
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+  const result = BMC(weight, height);
+  const message = `Your BMC is ${result}`;
+
+  Modal.message.innerText = message;
+  Modal.open();
 };
+
+Modal.buttonClose.onclick = () => Modal.close(); //Sem chaves pq a função so tem uma linha
+
+function BMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2);
+}
